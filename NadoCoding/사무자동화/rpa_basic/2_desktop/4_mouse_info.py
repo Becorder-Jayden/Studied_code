9b6e34f2229b06f22cb0d4a99bfd515b3aeae52e
Becorder-Jayden <starlight3775@gmail.com>
2021-11-02 22:01:33 +0900

    사무자동화

--- NadoCoding/사무자동화/rpa_basic/2_desktop/4_mouse_info.py
+++ NadoCoding/사무자동화/rpa_basic/2_desktop/4_mouse_info.py
@@ -5,3 +5,4 @@ pyautogui.PAUSE = 1 # 모든 동작에 1초씩 sleep 적용
 
 for i in range(5):
     pyautogui.move(100, 100)
+
---
+++ NadoCoding/사무자동화/rpa_basic/3_Web/3_selenium.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome("./chromedriver.exe")
+
+browser.get("http://daum.net")
