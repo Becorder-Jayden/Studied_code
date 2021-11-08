6f59f7cb56512409711c30cd2731800c40bbb7e2
Becorder-Jayden <starlight3775@gmail.com>
2021-11-08 11:08:10 +0900

    nadocoding cloning

--- NadoCoding/사무자동화/rpa_basic/3_Web/3_selenium.py
+++ NadoCoding/사무자동화/rpa_basic/3_Web/3_selenium.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome("./chromedriver.exe")
+browser = webdriver.Chrome(r"C:/Users/gmkim/Documents/GitHub/Studied_code/NadoCoding/사무자동화/chromedriver.exe")
 
 browser.get("http://daum.net")
