d3a91e6cc6da5b681b4646c7acbda4cec61b98b1
Becorder-Jayden <starlight3775@gmail.com>
2022-03-24 09:41:03 +0900

    ch_09,10

---
+++ ezenacademy/Java/src/chater2/DoubleEx2.java
@@ -0,0 +1,10 @@
+package chater2;
+
+public class DoubleEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ezenacademy/Java/src/chater2/Variable1.java
@@ -0,0 +1,11 @@
+package chater2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level;
+		level = 10;
+		System.out.println(level);
+	}
+
+}
